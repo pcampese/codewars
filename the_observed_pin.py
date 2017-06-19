6288f7a232f1d0671b60832971daca51d8d2a5ce
Paul Campese <paul.campese@simplivity.com>
2017-06-19 09:28:21 -0400

    Signed-off-by: Paul Campese <paul.campese@simplivity.com>

--- the_observed_pin.py
+++ the_observed_pin.py
@@ -1,4 +1,4 @@
-# https://www.codewars.com/kata/5263c6999e0f40dee200059d/train/python
+# https://www.codewars.com/kata/the-observed-pin/python
 
 def get_pins(observed):
     # Display the observed bin
