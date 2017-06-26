e3d8e610311c2000ad78d5b7075e0e8434a403d9
Paul Campese <paul.campese@simplivity.com>
2017-06-26 14:10:27 -0400

    Signed-off-by: Paul Campese <paul.campese@simplivity.com>

--- generate_bc.py
+++ generate_bc.py
@@ -1,14 +1,43 @@
 def generate_bc(url, separator):
-	import cgi
 	import re
 
+	# Display variables that are passed in to the method
 	print('url = [{}]'.format(url))
 	separator = separator.strip()	# Remove whitespace before/after the separator
 	print('separator = [{}]'.format(separator))
 	print
 
+	# Split the url into it's components, separate by '/'
+	print('+ Split the url')
 	url_components = url.split('/')
 	print('url_components = {}'.format(url_components))
+	print
+
+	# Replace all '-'s with spaces (for url elements that are less than 30 characters)
+	print("Replace - with ' '")
+	for index in range(len(url_components)):
+		value = url_components[index]
+		if (len(value) < 30):
+			url_components[index] = value.replace('-', ' ')
+	print('url_components = {}'.format(url_components))
+	print
+
+	# Merge back any http or https links that were broken during the "split on '/'" operation earlier
+	# Example, https://www.site.com should stay together, instead of
+	# being split into: http:, '', www.site.com
+	print('Merge back links starting with http or https')
+	if (url_components[0] == 'http:' or url_components[0] == 'https:'):
+		url_components[0] += url_components.pop(1)
+		url_components[0] += '//'
+		url_components[0] += url_components.pop(1)
+	print('url_components = {}'.format(url_components))
+	print
+	
+	# Remove any trailing empty '' (i.e. created due to a trailing / at the end: www.home.com/)
+	print("Remove any trailing ''")
+	url_components = [element for element in url_components if element]
+	print('url_components = {}'.format(url_components))
+	print
 
 	# print('url_components[0]: {} --> Trash'.format(url_components[0]))
 	# print('url_components[1]: {} --> Keep both UPPER and lower versions of it'.format(url_components[1]))
@@ -26,37 +55,59 @@ def generate_bc(url, separator):
 	if (html == 'INDEX'):
 		url_components.pop()
 		html = url_components[-1].upper()
-
 	print
 
+	# Reference list of HTML tags to use for formatting output
 	html_tags = [
 		'<a href="/">HOME</a> ',
-#		separator,
 		' <a href="/',
-#		url_components[1],
 		'/">',
-#		url_components[1].upper(),
 		'</a> ',
-#		separator,
-		' <span class="active">',
-#		html,
+		'<span class="active">',
 		'</span>'
 	]
-	
-	# Format the output
-	# Process the first element in the array the sameway
-	beginning = '<a href="/">HOME</a> '
-	# Repeat the middle items the same way
-	middle = ''
-	memory = ''
-	for element in url_components[1:-1]:
-		print('element = {}'.format(element))
-		memory += element + '/'
-		middle += ''.join([separator, ' <a href="/', memory, '">', element.upper(), '</a> ',])
-	# Process the last element the same way
-	end = ''.join([separator, ' <span class="active">', html, '</span>'])
 
-	result = ''.join([beginning, middle, end])
+	# If the number of url_components is 1, do special formatting...
+	if (len(url_components) == 1 ):
+		print('+ Length = 1')
+		result = html_tags[4] + 'HOME' + html_tags[5]
+	else:
+		# Format the output
+		# Process the first element in the array the sameway
+		print('+ Processing: Beginning')
+		beginning = '<a href="/">HOME</a> '
+
+		# Repeat the middle items the same way
+		print('+ Processing: Middle')
+		middle = ''
+		tag = ''
+		for element in url_components[1:-1]:
+			print('element = {}'.format(element))
+			# If the element is longer than 30 characters, then it needs special processing
+			length = len(element)
+			if (length >= 30):
+				print('++ Special Processing for length: [{}]'.format(length))
+				element_list = element.split('-')
+				value = ''
+				print('++ element_list = {}'.format(element_list))
+				# Remove words from this list
+				removal_list = ["the","of","in","from","by","with","and", "or", "for", "to", "at", "a"]
+				for word in element_list:
+					if word not in removal_list:
+						print('+++ Process word: [{}]'.format(word))
+						value += word[0]
+				print('++ value: [{}]'.format(value))
+			else:
+				value = element
+			tag += element + '/'
+			middle += ''.join([separator, ' <a href="/', tag, '">', value.upper(), '</a> ',])
+
+		# Process the last element the same way
+		print('+ Processing: End')
+		end = ''.join([separator, ' <span class="active">', html, '</span>'])
+
+		# Prepare the final result
+		result = ''.join([beginning, middle, end])
 	print('result = {}'.format(result))
 
 	return result
