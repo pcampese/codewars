6a6467ba5f69af8074c7bbfb99acd7aa17aaf64d
Paul Campese <paul.campese@simplivity.com>
2017-06-27 16:22:16 -0400

    Signed-off-by: Paul Campese <paul.campese@simplivity.com>

---
+++ isValidWalk.py
@@ -0,0 +1,11 @@
+def isValidWalk(walk):
+	# Display arguments
+	print('walk = {}'.format(walk))
+	
+	# First check to make sure that there are exactly 10 directions provided
+	if (len(walk) != 10):
+		result = False
+	else:
+		return True
+
+	return result
