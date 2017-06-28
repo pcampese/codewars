5f976b7d291fa742228d6207e86123c52828f884
Paul Campese <paul.campese@simplivity.com>
2017-06-28 09:48:19 -0400

    Signed-off-by: Paul Campese <paul.campese@simplivity.com>

--- dig_pow.py
+++ dig_pow.py
@@ -1,16 +1,22 @@
+# https://www.codewars.com/kata/playing-with-digits/train/python
+
 def dig_pow(n, p):
 	# Print arguments
 	print('n = number = {}'.format(n))
 	print('p = power = {}'.format(p))
 
 	# Calculate the digits to their powers
-	sum = 0
-	for base_power, digit in enumerate(str(n)):
-		exponent = base_power + p
-		print('{} ** {}'.format(digit, exponent))
-		sum = (int(digit) ** exponent) + sum
-		
-	# Display the sum
-	print('sum = {}'.format(sum))
+	sum = 0											# Variable to hold the rolling sum
+	for base_power, digit in enumerate(str(n)):		# Convert the number to a string.  Enumerate iterates and returns index too.
+		exponent = base_power + p					# Exponent we want is the digit's index + argument 'p'
+		sum = (int(digit) ** exponent) + sum		# Add digit ** exponent to the rolling sum
 
-	return -1
+	# Find a divisor for this sum
+	if (sum % n == 0):		# If the sum is divisible by the argument 'n', that was passed
+		result = sum / n	# Then the answer is the sum divided by the argument 'n'
+	else:					# Otherwise
+		result = -1			# Then return '-1'
+
+	print('result = {}'.format(result))
+
+	return result
