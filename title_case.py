9d5dd7146946e866ef3b1581b37c88d4d2869c84
Paul Campese <paul.campese@simplivity.com>
2017-07-10 16:41:58 -0400

    Signed-off-by: Paul Campese <paul.campese@simplivity.com>

--- title_case.py
+++ title_case.py
@@ -4,6 +4,15 @@ def title_case(title, minor_words=None):
 	# Print arguments
 	print('title = {}'.format(title))
 	print('minor_words = {}'.format(minor_words))
+	print
+
+	# Convert the provided arguments into lists
+	title = title.split()
+	if (minor_words != None):
+		minor_words = minor_words.split()
+	print('title = {}'.format(title))
+	print('minor_words = {}'.format(minor_words))
+	print
 
 	# Store the final result
 	result = 0
